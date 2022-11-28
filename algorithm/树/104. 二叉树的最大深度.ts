7eb3bca0133f604b270102a95287d60018b99aeb
zhengchengshi <2650744390@qq.com>
2022-11-28 21:04:40 +0800

    feat(algorithm): 添加二叉树题目

---
+++ algorithm/树/104. 二叉树的最大深度.ts
@@ -0,0 +1,51 @@
+/**
+ * 给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+ */
+/**
+ * Definition for a binary tree node.
+ *  */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+// 先序遍历
+function reverse(root: TreeNode | null, depth: number): number {
+  if (!root) return depth;
+  return Math.max(
+    reverse(root.left, depth + 1),
+    reverse(root.right, depth + 1)
+  );
+}
+function maxDepth(root: TreeNode | null): number {
+  const depth = reverse(root, 0);
+  return depth;
+}
+
+// 后序遍历
+function maxDepth2(root: TreeNode | null): number {
+  if (!root) return 0;
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+  const res = Math.max(leftDepth, rightDepth) + 1;
+  return res;
+}
---
+++ algorithm/树/543. 二叉树的直径.ts
@@ -0,0 +1,41 @@
+/**
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+
+ 
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ */
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+let ans: number;
+function getMaxLength(root: TreeNode): number {
+  if (root === null) return 0;
+  const leftDepth = getMaxLength(root.left!);
+  const rightDepth = getMaxLength(root.right!);
+  ans = Math.max(res, leftDepth + rightDepth + 1);
+  return Math.max(leftDepth, rightDepth) + 1;
+}
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  ans = 1;
+  getMaxLength(root as TreeNode);
+  return res - 1;
+}
