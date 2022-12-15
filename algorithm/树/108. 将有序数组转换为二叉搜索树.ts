5a309634a0f173512a822a0d6e2860d2ff82beb9
zhengchengshi <2650744390@qq.com>
2022-12-15 17:54:54 +0800

    feat(algorithm): 添加二叉树相关题目

---
+++ algorithm/树/108. 将有序数组转换为二叉搜索树.ts
@@ -0,0 +1,47 @@
+/**
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+
+高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+
+ 
+
+示例 1：
+
+
+输入：nums = [-10,-3,0,5,9]
+输出：[0,-3,9,-10,null,5]
+解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+
+示例 2：
+
+
+输入：nums = [1,3]
+输出：[3,1]
+解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+
+ */
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  if (!nums.length) return null;
+  // 关键点在于不能裁剪数组
+  const pivot = nums[nums.length >> 1];
+  const node = new TreeNode(pivot);
+  let leftNode = sortedArrayToBST(nums.slice(0, nums.length >> 1));
+  let rightNode = sortedArrayToBST(nums.slice((nums.length >> 1) + 1));
+  node.left = leftNode;
+  node.right = rightNode;
+  return node;
+}
