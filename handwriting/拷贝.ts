c78354a25b48702cd9e8f242d2e3922d827fa79e
zhengchengshi <2650744390@qq.com>
2022-12-28 21:20:35 +0800

    feat(handwriting): 添加深拷贝

--- handwriting/拷贝.ts
+++ handwriting/拷贝.ts
@@ -30,3 +30,21 @@ const arr = [1, 2, { name: "test" }];
 const copyArr1 = Array.from(arr);
 const copyArr2 = [...arr];
 const copyArr3 = arr.concat([]);
+
+const deepClone = (obj: any) => {
+  const temp = {};
+  if (typeof obj === "function") return obj;
+  if (obj instanceof RegExp) return new RegExp(obj);
+  if (obj instanceof Date) return new Date(obj);
+  // @ts-ignore
+  for (const attr in obj) {
+    console.log(attr);
+    // @ts-ignore
+    temp[attr] = obj[attr] instanceof Object ? deepClone(obj[attr]) : obj[attr];
+  }
+  return temp;
+};
+const deepCloneObj = deepClone(obj);
+// @ts-ignore
+obj.info.address = "yyy";
+console.log(deepCloneObj);
